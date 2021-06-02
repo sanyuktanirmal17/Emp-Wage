a9550d3feee3160006d89e7e6fe96753b2f6f954
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-02 19:52:04 +0530

    Added: Emp wage computation

--- EmpWage.java
+++ EmpWage.java
@@ -2,7 +2,15 @@
 class EmpWage {
         public static void main(String args[]) {
                 
+                 int fullTime = 1;
+                double empCheck= Math.floor(Math.random()*10)%2;
                 System.out.println("Welcome to employee wage computation");
+                        if (fullTime == empCheck)
+
+                                System.out.println("Emp is present");
+                        else
+                                System.out.println("Emp is absent");
+
                 
        }
 }
