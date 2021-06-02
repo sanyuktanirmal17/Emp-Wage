5d3f57dda744cc573d8cbefa0163f6dbab6286b7
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-02 19:47:16 +0530

    Added: Emp wage

---
+++ EmpWage.java
@@ -0,0 +1,8 @@
+
+class EmpWage {
+        public static void main(String args[]) {
+                
+                System.out.println("Welcome to employee wage computation");
+                
+       }
+}
