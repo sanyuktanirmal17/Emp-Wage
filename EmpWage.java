c1122bac7ca640a415873809fe2c32dd9ce61b64
sanyuktanirmal17 <68991535+sanyuktanirmal17@users.noreply.github.com>
2021-06-03 22:09:15 +0530

    Update EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,20 +1,28 @@
 package empBuilder;
 
-public class EmpWage {
-	public static void main(String args[]) {
-		int IS_FULL_TIME=1;
-		int EMP_RATE_PER_HOUR =20;
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck= Math.floor(Math.random()*10)%2;
-		System.out.println("Welcome to employee wage computation");
-			if (IS_FULL_TIME == empCheck)
-				empHrs = 8;
-			else
-					empHrs = 0;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage:" + empWage);
-		}
-		
- 	}
+public class Employee {
+        public static void main(String args[]) {
+        public static final int IS_FULL_TIME=1;
+        public static final int IS_PART_TIME=2;
+        public static final int EMP_RATE_PER_HOUR =20;
+                int empHrs = 0;
+                int empWage = 0;
+                double empCheck= Math.floor(Math.random()*10)%2;
+                System.out.println("Welcome to employee wage computation");
+                switch (empCheck) {
+                        case IS_PART_TIME:
+                                empHrs = 4;
+                                break;
+                        case IS_FULL_TIME:
+                                empHrs = 8;
+                                break;
+                        default:
+                                empHrs=0;
+                }
+                        empWage = empHrs * EMP_RATE_PER_HOUR;
+                        System.out.println("Emp Wage:" + empWage);
+                }
+
+        }
+
 
